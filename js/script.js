3255d915653582d2fb071b9f2c20666e835e7f5e
gabrieleomegna <gabrieleomegna01@gmail.com>
2023-11-27 18:44:42 +0100

    Implemented function gridGenerator, implemented event listener for the button 'play'

--- js/script.js
+++ js/script.js
@@ -4,3 +4,37 @@
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro 
 // ed emetto un messaggio in console con il numero della cella cliccata.
 
+
+// ! Aggiungo un EventListener al bottone play
+  // ! genero una griglia quadrata 10x10
+
+const wrapperElement = document.querySelector('div.wrapper');
+
+const btnPlayElement = document.querySelector('button#play');
+const gridElement = gridGenerator (100, wrapperElement);
+
+btnPlayElement.addEventListener('click',
+function () {
+    gridElement.classList.toggle('display-none')
+})
+
+
+
+
+
+
+// ! Aggiungo un event listener per ogni cella in cui:
+  // ! la cella si colora di azzurro
+  // ! emetto un messaggio in console in cui viene scritto il numero della cella corrispondente
+
+
+// wrapperElement.appendChild(gridGenerator(100));
+
+function gridGenerator(numberOfSquares, parent) {
+    for (let i = 0; i < numberOfSquares; i++) {
+        const squareElement = document.createElement('div');
+        squareElement.classList.toggle('square-item');
+        squareElement.classList.toggle('display-none');
+        parent.appendChild(squareElement);
+    }
+}
